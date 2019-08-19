e4c93fcffa0728e1504ea8f3f0baaa8c22418a66
SierraOG <sierrao@utexas.edu>
2019-08-19 16:37:56 -0500

    tested display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,7 @@
 // Test away!
 import React from "react";
 import renderer from "react-test-renderer"; 
+import { toHaveClass } from '@testing-library/jest-dom';
 import { render, fireEvent } from "@testing-library/react";
 
 import Display from './Display'
@@ -12,11 +13,24 @@ describe("<Display />", () => {
         // snapshots are a JSON representation of the DOM tree
         expect(tree.toJSON()).toMatchSnapshot();
     });
-    it("defaults to unlocked and opened", () => {
-        const { findByText } = render(<Display />);
+    it("displays open if open and unlocked if unlocked", () => {
+        const { findByText } = render(<Display closed={false} locked={false}/>);
         expect(findByText(/open/i)).toBeDefined
-        expect(findByText(/unlocked/i)).toBeDefined
-        expect(findByText(/closed/i)).toBeUndefined
-        expect(findByText(/locked/i)).toBeUndefined
+        expect(findByText(/unlocked/i)).toBeDefined;
+    })
+    it("displays closed if closed and locked if locked", () => {
+        const { findByText } = render(<Display closed={true} locked={true}/>);
+        expect(findByText(/closed/i)).toBeDefined
+        expect(findByText(/locked/i)).toBeDefined
+    })
+    it("when closed and locked both buttons should have class name red-led", () => {
+        const { getByText } = render(<Display closed={true} locked={true}/>);
+        expect(getByText(/closed/i).classList[1]).toEqual('red-led')
+        expect(getByText(/locked/i).classList[1]).toEqual('red-led')
+    })
+    it("when open and unlocked both buttons should have class name green-led", () => {
+        const { getByText } = render(<Display open={true} unlocked={true}/>);
+        expect(getByText(/open/i).classList[1]).toEqual('green-led')
+        expect(getByText(/unlocked/i).classList[1]).toEqual('green-led')
     })
 })
